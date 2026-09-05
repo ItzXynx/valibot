98ef8b4efc7858c210926c47cf515604b0c7a6f2
Fabian Hiller <hillerfabian11@gmail.com>
2026-09-05 19:12:45 -0400

    Improve perf by changing ~standard implementation (#1534)
    * Improve perf by changing ~standard implementation
    
    * Rename _addStandardProp to _standardSchema
    
    * Refine Standard Schema initialization types and coverage
    
    ---------
    
    Co-authored-by: Yaroslav Slepukhin <YaroslavSlepukhin@gmail.com>

--- library/src/schemas/literal/literal.ts
+++ library/src/schemas/literal/literal.ts
@@ -4,7 +4,7 @@ import type {
   ErrorMessage,
   OutputDataset,
 } from '../../types/index.ts';
-import { _addIssue, _getStandardProps, _stringify } from '../../utils/index.ts';
+import { _addIssue, _standardSchema, _stringify } from '../../utils/index.ts';
 
 /**
  * Literal type.
@@ -83,7 +83,7 @@ export function literal(
   literal_: Literal,
   message?: ErrorMessage<LiteralIssue>
 ): LiteralSchema<Literal, ErrorMessage<LiteralIssue> | undefined> {
-  return {
+  return _standardSchema({
     kind: 'schema',
     type: 'literal',
     reference: literal,
@@ -91,9 +91,6 @@ export function literal(
     async: false,
     literal: literal_,
     message,
-    get '~standard'() {
-      return _getStandardProps(this);
-    },
     '~run'(dataset, config) {
       if (dataset.value === this.literal) {
         // @ts-expect-error
@@ -104,5 +101,5 @@ export function literal(
       // @ts-expect-error
       return dataset as OutputDataset<Literal, LiteralIssue>;
     },
-  };
+  });
 }
